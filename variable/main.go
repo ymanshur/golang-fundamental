2347555d80e0d9bbe39ea242928b9ca0be4115c2
Yusuf Manshur <ymanshur@gmail.com>
2022-09-12 14:21:04 +0700

    Added variable concept module

---
+++ variable/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Golang"
+	age := 20 // Static data type
+	age = 30
+	// var name string
+	// name = "Ruby on Rails"
+
+	fmt.Println(name)
+	fmt.Println(age)
+}
