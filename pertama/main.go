078cc6e5986ac2a69e91ef86876bc28c147a2212
Yusuf Manshur <ymanshur@gmail.com>
2022-09-12 13:38:08 +0700

    Package and main func concept (2)

--- pertama/main.go
+++ pertama/main.go
@@ -1,10 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"pertama/calculation"
+)
 
 func main() {
 	fmt.Println("Halo, belajar Golang")
-	sentence := TestAja()
+	// sentence := TestAja()
 
-	fmt.Println(sentence)
+	// fmt.Println(sentence)
+
+	result := calculation.Add(9, 9)
+
+	fmt.Println(result)
 }
